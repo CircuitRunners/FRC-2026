055a631bb09ddbaebe44ec35f67f856814a9002c
Anvita Bisen <anvitabisen@gmail.com>
2026-01-30 18:22:24 -0500

    Fixed some stuff

--- src/main/java/frc/robot/subsystems/Kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/Kicker/Kicker.java
@@ -3,15 +3,14 @@ package frc.robot.subsystems.Kicker;
     import frc.lib.bases.MotorSubsystem;
     import frc.lib.io.MotorIOTalonFX;
     import frc.lib.io.MotorIO.Setpoint;
-    import frc.robot.subsystems.intakeRollers.IntakeRollerConstants;
 
 public class Kicker extends MotorSubsystem<MotorIOTalonFX>{
 
         public static final Setpoint IDLE = Setpoint.withNeutralSetpoint();
-        public static final Setpoint FEED_FORWARD = Setpoint.withVoltageSetpoint(IntakeRollerConstants.kIntakeVoltage);
-        public static final Setpoint FEED_BACKWARDS = Setpoint.withVoltageSetpoint(IntakeRollerConstants.kExhaustVoltage);
-        public static final Setpoint HOLD_PIECE = Setpoint.withVoltageSetpoint(IntakeRollerConstants.kPelicanVoltage);
-        public static final Setpoint START = Setpoint.withVoltageSetpoint(IntakeRollerConstants.kStartVoltage);
+        public static final Setpoint FEED_FORWARD = Setpoint.withVoltageSetpoint(KickerConstants.kFeedForwardVoltage);
+        public static final Setpoint FEED_BACKWARDS = Setpoint.withVoltageSetpoint(KickerConstants.kFeedBackwardVoltage);
+        public static final Setpoint HOLD_PIECE = Setpoint.withVoltageSetpoint(KickerConstants.kHoldVoltage);
+        public static final Setpoint START = Setpoint.withVoltageSetpoint(KickerConstants.kStartVoltage);
     
 public Kicker() {
     super(KickerConstants.getMotorIO(), "Kicker");
